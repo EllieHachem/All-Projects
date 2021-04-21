29c4ecf895544a5fb04471a7c7ff704a43f1662e
Ellie Hachem <82949298+EllieHachem@users.noreply.github.com>
2021-04-21 20:40:27 +0200

    Add files via upload

---
+++ day-65-interviewith-building-op-site-4-pillars-of-epic-site/note.py
@@ -0,0 +1,156 @@
+# today we talk about designing op sites dont judj a book about its covr and u only hav  3 seconds to make an expression it is just lik job interviw 
+#if website looks bad how much u put for product prices? website makees u appear rich 
+#designe is one of cheapeet wawys to make ur product go up 
+
+#good web design is 4 
+#color theory / typography/user expreince / ui design 
+#art and scinece on using right color for websitee 
+#just fliping color can change everything 
+#color palate  = color design = color taste
+#dominantee color there is 5 types of color 
+#see the image I will upload it here 
+#red = love /energy/intensity like car need red cuz u will feeel energtic if u buy thee car or love 
+#yelloww = joy / intelect /attention like sonic so shiny it is impossible to negate yellow add adding it with a girl in the scence hmm /headline /logo not for looked up for long period of time it will hurt user eyee reading all article in yellow 
+#green frishness/growth/saftey like grocery sites like for healthy food 
+#blue = stability /trust /sernity(good mentality) like paypal crypto company or even facbeook
+#final color is purple = roality,wealth,femenity 
+
+#combining color usually we ar gonna use more than one color 
+
+#which 2 color 2 combine 
+
+#this is most scinetifc part 
+
+#we must scientific way to combine color like taking 2 colors right next to each others on the color wheel = creaete analgous color palate they really work together like  good for navi bar or body of website or logo and background what is not so good = on standing out 
+#but to stand out we need to use complementary color = 2 opposite color fro mthe wheel = clashing palatee like both will work on us 
+#complementary color like green and red what u shouldnt do with clashing colors or complementary is styling text and text background with it so dont do it for text do it for logos or icons or things that want to stand out 
+#drawing traidg color palate or perfect square go  adobe color 
+#it let u mess with combo of colors or use color haunt and take code straight take look at popluar section and use it for ur next project 
+
+# #Modern color theory uses either the RGB additive color model or the CMY subtractive color model, and in these, the complementary pairs are red–cyan, green–magenta, and blue–yellow.
+# In the traditional RYB color model, the complementary color pairs are red–green, yellow–purple, and blue–orange.
+
+#typography like way of writing codes like if u want to send love letteer proper font is a must 
+
+#there is 2 fonts u should know about one is serif family  they have feeet at end of edges of letter = design more series cuz when carving stone it is hard to have 90% degree = older for letter head for company or magzine for atriticaul design but serif is designedd to many sup family like traditional,old style,modren/slab serif 
+#compare the  in each of the types to compare them 
+#modren  + athrotive = go modren seerif 
+#to appear old go old which is for us 
+#different fonts have different mods image is uploaded here 
+#the o express the difference between edges of how letter written 
+#sans serif are differeent families cuz instead of having feet they have perfrt angels = sans are like delicious
+#human san serif is a sub family of sans and is highly readable and sans have many sub families 
+#sans serif = used mostly for boddy = essay 
+#how easy words to read and how pleasent it is to ready 
+#just changing font based on studies can save 30% or or even 40% of time that is why change all note pads later to better background + font to read easy so humanist sans serif is best  uses in sales pitchs 
+#also 4 paterns are important gonna uplaod image here 
+
+#dont use a lot of fonts just max 2 fonts in one design = alot cleaner and tigher 
+#and when combining though 2 different fonts = find similar mode since each font have mod so they must be nearly the same and time era 
+#like new type of font with new not old with knew 
+
+#contrast like heading serif and body san serif or opposite way around 
+#change weight between haeding and body = where heading really really bold even using black type face and body thin or regular or light ddifferent weigth can create contrast 
+
+#see image list of typee of fonts to avoid 
+
+#now Useer interface design 
+
+#we are alwways draw to the bigger balder square things we can use this knoweldge to define hirarcy 
+
+#lets say u want to do birthday invtie to jane if all text are the same it wil take  user a lot longer but using the hiracy like making the most improtant words as bold and huge will make people notice 
+#so if all background is blue and jsut text is weight that is good cuz u want people to focus on one thing thati s why eeveen in font or color jsut 2 max usign science ofcourse or make 2 colors then just button  color is different to gain money maybe addding a button to buy something from site 
+#bigger item = draw eyes to it 
+
+#hirracry the layout of website improtant 
+
+#wikipedia are example of bad design 
+
+#like even line should be medium lenth too short or long is bad 
+
+#layout should have iamges andd each have different size and stuff uniquee not just text and paste explains why notepad in job she said hard to read like u aim for 40,60 chara per line 
+
+#good layout example like grammerly website it chops website like many functions making it easy for u to access like refactoring properly 
+
+
+#alignement how positon each element on screen relative to each others see image of anlightment like both should be left not title center and text left ti will be wierd 
+
+#reduce number of alignement points 
+
+#try to make design like that of the image shown all in one line 
+
+#visual starting elemeent line that is why using grids is important to have proper alignement 
+
+#see the new image to see just howw alignment can make difference in design 
+
+#white space = empty space around elements or text 
+
+#see image of good bag shop and bad and compare which one u will buy from like that shows margin and padding example then they look more expensive  
+
+#like nosie candling product many elements images near earphone no one buy it but making space it will appear as apple see image too
+
+#last thing is audience and most important 
+#design for ur audience
+
+#like see the image childdren concret the first one is so serious children wil go to 2nd one 
+
+#thing what will appeal to them even if bit messy and bolder as long as it fits the audeince is okay 
+
+#so keep in mind 
+
+#hirarcy 
+#layout
+#alignment
+#whitespace
+#auidence 
+
+#now user expreince = ux 
+#imagine a collge reforming compass  
+
+#it must  be like see everything easy like facebook just add and post and comment and chat easily 
+#thing of 5 things 
+#simplitcity dont go complex 
+#consistency like make things the same in every part of website like xinfity is website to give internet and tv to many people but eaach site havee differennt style making it harder for people as many of their users are adults 
+#peeople reading patterns #most common patter is f patterns where the eyes start at top left corner then go right and do the same use f layout most important things on the left side of screen hmm logo and important on left and right simple images cuz they will read the left mostly 
+#another common pattern is z pattern left to right but then zig zag to left right like moving in path of z they will look 4 types and used on fb so ya  fb is best site to learn from see the image 
+
+#5th all platform design think of mobile morely so start with mobile always then go laptop 
+
+#try to reduce banners like advertinsg ads and cookies alert those really must u know be concsierded in a site dont do opposite just 1  part is bad too see image of  banner and 1 image 
+#test site many times like at least family and get feedback how to improve design espically if u can not afford professional testers 
+
+#dont use your power for evil = dark pattern = patterns that get usr to perfrom an action or behavoir that is benefiecal for company or designer but not necesary for what user want = another form of bad user expreince 
+
+#example of real life like u bough a sandwidch that appear to have 2 long parts then the part was just air covered by carton (see the image)
+#image name is evil pattern 
+#in digital word they do the same 
+#lets see the amazone trick 
+#when u want to buy normal delivery or express they highlight the express in orange so u ar tricked to pay more for this delivery 
+
+#or another dark pattern is add of shoes then they put a piece of curve that appear as hair then u try to remove it but it goes to thir site and make them earn mony lol se image see the shoes trick imag  
+
+# good design is helping people what they want to do and giving them good expreinc while thy at it 
+
+#the biggest evil among all is ryanair where thy  try to charge u for anything they offer lik they even hidde dont buy insurance so that u accidenly buy it 
+#also thos chck boxs that tells u if u want to stop having deals it is one of most evil patterns see the image how long and confusing = that is bad expreince see image namd chck box trick 
+
+# pillars = ruls 
+ 
+# daily ui allow us to design every day for 100 day = improve dsigner skills by reption
+#go to canvas = online editing sit = ik how to us it 
+#dimntion pxil = wight *hight use 1400*800
+#so make 3 pages and each page is sit pages 
+
+#like serif typ fac is cool to for hotel it mak thm appar old nough u knoww thos feet at end so create an anlightment + sub heading with contrast 
+
+#framing picture is cool like put circle that have outline  on top of object each image should proper spacing + images can be feastures not just icon/glips remember text under the image should be unlined too 
+#canvas have spacing option that is epic 
+#lorem empsim remeber it is the auto generator even her text under image is not too long take image or see video of her prototype 
+#see her first image cleary she want us to see the a hotel word 
+#third image is consisntancy same topography color palate so ya 
+#the straight line is the alignment in menu tool  or if u see line but most of the time we need to play with the design to fix stuff as alignement 
+#so if all element have right left or mid aligment and all must be same if one is right then other is right and so on 
+#train website design by thumbnaile design in canvas there is option to publish as website pretty epic = that is cool for mockup or prototype cuz it not able to have javascript to it or etc 
+
+#so if adding elemnt on top of each others use analogus color so that ya that is why they are used 
+
